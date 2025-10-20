adb0412b151817895f1e520f999a1e993456e193
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-10-20 18:28:04 +0200

    Initial local commit

---
+++ src/main/java/org/matita08/plugins/clanPlugin/ClanPlugin.java
@@ -0,0 +1,17 @@
+package org.matita08.plugins.clanPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClanPlugin extends JavaPlugin {
+   
+   @Override
+   public void onEnable() {
+      // Plugin startup logic
+      
+   }
+   
+   @Override
+   public void onDisable() {
+      // Plugin shutdown logic
+   }
+}
