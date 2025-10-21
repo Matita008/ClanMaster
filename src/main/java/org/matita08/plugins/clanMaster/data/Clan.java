186cc6ce41906918e22ac3e55417b7f77940d2e0
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-10-21 23:11:05 +0200

    Created Clan class, fixed issue with ClanPlugin.getConfig
    Signed-off-by: Matita008 <155455608+Matita008@users.noreply.github.com>

--- src/main/java/org/matita08/plugins/clanMaster/data/Clan.java
+++ src/main/java/org/matita08/plugins/clanMaster/data/Clan.java
@@ -1,4 +1,38 @@
 package org.matita08.plugins.clanMaster.data;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Clan {
+   @Getter private final String name;
+   @Getter private final String tag;
+   private final Set<Member> members;
+   @Getter private Member owner;
+   
+   public Clan(String name, String tag, Member... members) {
+      this.name = name;
+      this.tag = tag;
+      this.members = new HashSet<>(List.of(members));
+      if(Arrays.stream(members).filter(m -> m.getRank().equals(Rank.Owner)).count() !=1 )
+         throw new AssertionError("The member array passed " + Arrays.toString(members) + " Does not contain a single owner");
+      
+      //noinspection OptionalGetWithoutIsPresent // the above check ensure that there is 1 owner
+      this.owner = Arrays.stream(members).filter(m -> m.getRank().equals(Rank.Owner)).findFirst().get();
+   }
+   
+   public Clan(String name, String tag, Member owner) {
+      this.name = name;
+      this.tag = tag;
+      this.owner = owner;
+      members = new HashSet<>();
+   }
+   
+   public void addMember(Member m){ members.add(m); }
+   
+   public void removeMember(Member m) { members.remove(m); }
+   
 }
