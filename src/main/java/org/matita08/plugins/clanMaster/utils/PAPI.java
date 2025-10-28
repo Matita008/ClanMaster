ecbd2f586ab16bda11a21685b6a9c7af2882636d
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-10-28 22:02:21 +0100

    Implemented PAPI hook
    Signed-off-by: Matita008 <155455608+Matita008@users.noreply.github.com>

--- src/main/java/org/matita08/plugins/clanMaster/utils/PAPI.java
+++ src/main/java/org/matita08/plugins/clanMaster/utils/PAPI.java
@@ -1,30 +1,59 @@
 package org.matita08.plugins.clanMaster.utils;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.matita08.plugins.clanMaster.ClanPlugin;
+import org.matita08.plugins.clanMaster.data.Member;
 
 public class PAPI extends PlaceholderExpansion {
+   static final ClanPlugin pl = ClanPlugin.getInstance();
+   private static PAPI instance;
+   
    @Override
    public @NotNull String getIdentifier() {
-      return "ClanMaster";
+      return "clans";
    }
    
    @Override
    public @NotNull String getAuthor() {
-      return ClanPlugin.getInstance().getDescription().getAuthors().toString();
+      return pl.getDescription().getAuthors().toString();
    }
    
    @Override
    public @NotNull String getVersion() {
-      return ClanPlugin.getInstance().getDescription().getVersion();
+      return pl.getDescription().getVersion();
    }
    
-   private static PAPI instance;
+   @Override
+   public boolean persist() {
+      return true;
+   }
+   
+   @Override
+   public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+      Member member = Member.getMember(player);
+      if(member == null) return null;
+      if(!member.isInClan()) return Strings.NO_CLAN;
+      
+      return switch(params) {
+         case "clan", "player_clan" -> member.getClan().getName();
+         case "tag", "player_tag" -> member.getClan().getTag();
+         case "rank", "player_rank", "role", "player_role" -> member.getRank().toString();
+         case "clan_members_online", "members_online" -> member.getClan().getMembers().stream().filter(m -> m.getPlayer().isOnline()).count() + " online";
+         default -> null;
+      };
+   }
    
    public static synchronized void init() {
       if(instance != null) return;
       instance = new PAPI();
       instance.register();
+   }
+   
+   //TODO: crate i8n class, this is here to make future refactoring easier (i hope at least)
+   static final class Strings {
+      public static final String NO_CLAN = "not in a clan";
    }
 }
